1025c407450853ab7104f9d4c6d774c72ce5e3d9
aplawson7707 <aplawson7707@gmail.com>
2021-08-19 18:53:58 -0700

    Add method to standardize emails with and without street address 2

--- addressFormatter.js
+++ addressFormatter.js
@@ -17,9 +17,7 @@
   8:  Select Custom Scripts > Format Emails
   9:  Grant permissions when it prompts you to. (You'll only do this once per Google account)
   10: Run the script one more time if it does not run the first time
-  
-  Hopefully this helps, brother! This is just a first pass. I'm going to keep building this out.
-  Love ya, man!  
+ 
 */ 
 
 const ss = SpreadsheetApp.getActiveSpreadsheet();
@@ -59,16 +57,40 @@ function checkForOuput() {
 
 function formatAddresses() {
   var output = ss.getSheetByName("Formatted Addresses");
-  var range = input.getDataRange();
-  var values = range.getValues();
+  var values = input.getDataRange().getValues();
+  var name = values[0].indexOf("NAME");
+  var street1 = values[0].indexOf("Street");
+  var street2 =  values[0].indexOf("Street #2");
+  var city = values[0].indexOf("CITY");
+  var state = values[0].indexOf("STATE");
+  var zip = values[0].indexOf("ZIP");
   var destRange = output.getDataRange();
   destRange.clear();
-  var finalRange = output.getRange(1,1,values[0].length,values.length)
-  finalRange.setValues(Object.keys(values[0]).map ( function (columnNumber) {
-    return values.map( function (row) {
-      return row[columnNumber];
-    });
-  })); 
+  var nameArr = [];
+  var stArr = [];
+  var cityArr = [];
+  var stackedAddress = [];
+
+  values.filter (function (row) {
+    return (row[name] !== "");
+  })
+  .forEach(function (row) {
+    nameArr.push(row[name]);
+    if(row[street2] !== "") {
+      stArr.push(row[street1] + " " + row[street2]);
+    }
+    else {
+      stArr.push(row[street1]);
+    };
+    cityArr.push(row[city] + ", " + row[state] + " " + row[zip]);
+  });
+  stackedAddress.push([nameArr],[stArr], [cityArr]);
+  var nameRange = output.getRange(1, 1, 1, stackedAddress[0][0].length);
+  var stRange = output.getRange(2, 1, 1, stackedAddress[0][0].length);
+  var cityRange = output.getRange(3, 1, 1, stackedAddress[0][0].length);
+  nameRange.setValues(stackedAddress[0]);
+  stRange.setValues(stackedAddress[1]);
+  cityRange.setValues(stackedAddress[2]);
 }
 
 // Create function to set sheet formatting
@@ -80,4 +102,5 @@ function prettify() {
   destRange.setNumberFormat('@STRING@');
   output.autoResizeColumns(1, destValues[0].length);
   destRange.setHorizontalAlignment("center");
+  output.setFrozenColumns(1);
 }
